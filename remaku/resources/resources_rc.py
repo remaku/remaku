b3fe979d097e460fa5f15d9bb438a0885d15b2bc
Nelson Lai <me@nelsonlai.dev>
2026-06-07 18:16:59 +0800

    chore: compile rcc

--- remaku/resources/resources_rc.py
+++ remaku/resources/resources_rc.py
@@ -1189,13 +1189,10 @@ qt_resource_struct = b"\
 \x00\x00\x01\x9e\xa0\xb1\xfc\xea\
 "
 
-
 def qInitResources():
     QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
-
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
-
 
 qInitResources()
